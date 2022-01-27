71250d11c7ecbbe55678db372bf9f937daed30d5
nghia-nef <nghia14302@gmail.com>
2022-01-27 10:24:21 +0700

    refactor validation and make Regex validation

--- src/Lib/Regex.java
+++ src/Lib/Regex.java
@@ -1,5 +1,10 @@
 package Lib;
 
 public class Regex {
-    public static final String ID_PATTERN = "";
+    public static final String STUDENT_ID_PATTERN = "(SE|se)\\d{6}";
+    public static final String DATE_PATTERN = "yyyy-mm-dd";
+    public static final String TIME_PATTERN = "HH:mm";
+    public static final String EMAIL_PATTERN = "^(([^<>()[\\]\\.,;:\\s@\\\"]+(\\.[^<>()[\\]\\.,;:\\s@\\\"]+)*)|(\\\".+\\\"))@(([^<>()[\\]\\.,;:\\s@\\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\\"]{2,})$";
+    public static final String PASSWORD_PATTERN = "[\\w\\d\\D\\W\\s]{8,}";
+    
 }
