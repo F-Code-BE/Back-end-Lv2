8dbf1ebb65d87b46ecfab87451a4d03c5834d393
Ngo Duy Binh <duybinhngo2901@gmail.com>
2022-02-10 20:27:51 +0700

    Update input date and add feature to mark absent student.

--- src/controller/CheckAttendence.java
+++ src/controller/CheckAttendence.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import Lib.Validation;
-import model.Student;
 import patterns.Singleton;
 import view.FapMenu;
 
@@ -20,18 +19,21 @@ public class CheckAttendence {
     private String slotId;
     private int slot;
     private Date date;
-    private ArrayList<Student> students = new ArrayList<Student>();
+    private ArrayList<Date> dates = new ArrayList<>();
+    private ArrayList<String> studentIds = new ArrayList<>();
+
     public CheckAttendence(String teacherId) {
         this.teacherId = teacherId;
     }
 
-   
     private void getClassAndSlot() {
         var slots = new ArrayList<Integer>();
         var classIds = new ArrayList<String>();
+        var slotIds = new ArrayList<String>();
         Connection conn = Singleton.getInstance();
         try {
-            PreparedStatement stmt = conn.prepareStatement("select class_id, slot  from slot where teacher_id = ? and date = ?");
+            PreparedStatement stmt = conn
+                    .prepareStatement("select class_id, slot, id  from slot where teacher_id = ? and date = ?");
             stmt.setString(1, teacherId);
             var dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             String strDate = dateFormat.format(date);
@@ -40,9 +42,9 @@ public class CheckAttendence {
             while (rs.next()) {
                 classIds.add(rs.getString(1));
                 slots.add(rs.getInt(2));
+                slotIds.add(rs.getString(3));
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         var menu = new FapMenu();
@@ -51,7 +53,7 @@ public class CheckAttendence {
         int userChoice = menu.getUserChoice();
         classId = classIds.get(userChoice - 1);
         slot = slots.get(userChoice - 1);
-       
+        slotId = slotIds.get(userChoice - 1);
     }
 
     private void showStudentList() {
@@ -59,22 +61,93 @@ public class CheckAttendence {
         System.out.println("   List of your student");
         Connection conn = Singleton.getInstance();
         try {
-            PreparedStatement stmt = conn.prepareStatement("select Student.id, Student.name from Student where id in ( select student_id from Class_student where class_id = ?)");
+            PreparedStatement stmt = conn.prepareStatement(
+                    "select Student.id, Student.name from Student where id in ( select student_id from Class_student where class_id = ?)");
 
             stmt.setString(1, classId);
-           
+
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
-              System.out.println(rs.getString(1) + " " + rs.getString(2));
+                String studentId = rs.getString(1);
+                System.out.println(studentId + " " + rs.getString(2));
+                studentIds.add(studentId);
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+
+    private void getDates() {
+        Connection conn = Singleton.getInstance();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("select distinct date from slot where teacher_id = ? ");
+            stmt.setString(1, teacherId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                dates.add(rs.getDate(1));
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void checkPresent() {
+        Connection conn = Singleton.getInstance();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("update attendance set status = ? where slot_id = ?");
+            stmt.setString(1, "PRESENT");
+            stmt.setString(2, slotId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void checkAbsence() {
+        String absentStudent;
+        Connection conn = Singleton.getInstance();
+        while (true) {
+            absentStudent = Validation.inputString("Enter student id of absent student(0 to finish): ",
+                    "^SE[0-9]{6}$|^se[0-9]{6}$|^0$");
+            absentStudent = absentStudent.toUpperCase();
+            if (absentStudent.equals("0"))
+                break;
+            if (!studentIds.contains(absentStudent)) {
+                System.out.println("Student not int this class.");
+                continue;
+            }
+            try {
+                PreparedStatement stmt = conn
+                        .prepareStatement("update attendance set status = ? where slot_id = ? and student_id = ?");
+
+                stmt.setString(1, "ABSENT");
+                stmt.setString(2, slotId);
+                stmt.setString(3, absentStudent);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     public void showMenu() {
-       date = Validation.inputDate("Enter date you want to check: ");
-       getClassAndSlot();
-       showStudentList();
+        getDates();
+        var flag = false;
+        do {
+            date = Validation.inputDate("Enter date you want to check: ");
+            if (dates.contains(date)) {
+                flag = true;
+            } else {
+                System.out.println("You don't have any class schedule this date");
+            }
+        } while (!flag);
+        getClassAndSlot();
+        showStudentList();
+        checkPresent();
+        checkAbsence();
     }
 }
