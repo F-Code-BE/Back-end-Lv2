cb65c1f92e56dd9ca51e3788a46997253239ef3b
Hoa Van Phu <78528968+vphoa342@users.noreply.github.com>
2022-02-19 10:09:45 +0700

    Origin/feature/view info (#8)
    * view-info first commit
    
    * fix bug
    
    * fix input choice are invalid - viewInfo - viewAttendance
    
    * move view_info to main, create Timetable.java, modify changeGroup
    
    * refector view info - Main
    
    * merge branch Fix/refactor-view-info - Main
    
    Co-authored-by: Nghĩa Nef <trongnghia143@outlook.com.vn>

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,38 +3,56 @@ package view;
 import controller.*;
 
 public class Main {
+
     public static void main(String[] args) {
 
         while (true) {
             int userCase = LogIn.logIn();
             var menu = new FapMenu();
+            String userID = LogIn.getUserId();
             int userChoice;
             if (userCase == 1) {
                 menu.add("View information");
                 menu.add("View curriculum");
                 menu.add("View attendence report");
-                menu.add("View mark report");
                 menu.add("View academic transcript");
-                menu.add("View timetable");
+                menu.add("View Timetable");
                 menu.add("Change group");
                 menu.add("Suspend one semester");
                 menu.add("Change information");
                 menu.add("Request to ignore attendence");
                 menu.add("Log out");
                 do {
+                    ViewInfo viewInfo = new ViewInfo();
                     userChoice = menu.getUserChoice();
-                    System.out.println(LogIn.getUserId());  
+                    System.out.println(userID);
+                    
                     switch (userChoice) {
-                        case 7:
-                            ChangeGroup.getAllCourses(LogIn.getUserId());
+                        case 1:
+                            viewInfo.viewStudentInfo(userID);
+                            break;
+                        case 2:
+                            viewInfo.viewSyllabus(userID);
+                            break;
+                        case 3:
+                            viewInfo.viewAttendance(userID);
+                            break;
+                        case 4:
+                            viewInfo.viewMarkReport(userID);
+                            break;
+                        case 5:
+                            viewInfo.viewTimetable(userID);
+                            break;
+                        case 6:
+                            ChangeGroup.getAllCourses(userID);
                             break;
                         default:
                             break;
                     }
-                   
-                } while (userChoice != 11);
+
+                } while (userChoice != 10);
             } else if (userCase == 2) {
-                menu.add("View timetable");
+                menu.add("View Timetable");
                 menu.add("Check attendance");
                 menu.add("Change information");
                 menu.add("Change subject");
@@ -64,16 +82,18 @@ public class Main {
                 do {
                     userChoice = menu.getUserChoice();
                     switch (userChoice) {
-                        case 1: 
+                        case 1:
                             AccountModify.addingAccount();
                             break;
                         case 2:
                             AccountModify.deleteAccount();
-                            break;  
+                            break;
                         default:
                             break;
                     }
                 } while (userChoice != 4);
+            } else {
+                break;
             }
         }
     }
