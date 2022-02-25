101c54f526b459bc2e2877866c447da64d2cdc2a
Ngo Duy Binh <duybinhngo2901@gmail.com>
2022-02-25 23:10:40 +0700

    Change email regex.

--- src/Lib/Regex.java
+++ src/Lib/Regex.java
@@ -6,7 +6,7 @@ public class Regex {
     public static final String STUDENT_ID_PATTERN = "(SE|se)\\d{6}";
     public static final String DATE_PATTERN = "yyyy-MM-dd";
     public static final String TIME_PATTERN = "HH:mm";
-    public static final String EMAIL_PATTERN = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$";
+    public static final String EMAIL_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     public static final String PASSWORD_PATTERN = "[\\w\\d\\D\\W\\s]{3,}";
     public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_PATTERN);
 }
